ee91aed09c1b6701c90c185e0ca2c222513d4a1b
nellfs <pokycelit@gmail.com>
2022-07-31 19:43:26 -0300

    cleanup

--- src/containers/Calculator/index.tsx
+++ src/containers/Calculator/index.tsx
@@ -4,23 +4,37 @@ import "./index.css";
 import Button from "../../components/Button";
 import Display from "../../components/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 const Calculator = () => {
-  const [state, setState] = useState("test");
+  const addNumber = (n: number | string) => {
+    setState({ ...state, displayValue: state.displayValue + n });
+  };
+
+  const [state, setState] = useState({ ...initialState });
   return (
     <div className="calculator">
       <div className="display">
-        <Display value={state}></Display>
+        <Display value={state.displayValue}></Display>
       </div>
       <div className="buttons">
-        <Button label="del" /> <Button label="(" /> <Button label=")" />{" "}
-        <Button label="mod" />
-        <Button label="π" />
         <Button
-          label="7"
+          label="del"
           click={() => {
-            setState("working");
+            setState({ ...state, displayValue: "" });
           }}
         />
+        <Button label="(" />
+        <Button label=")" />
+        <Button label="mod" />
+        <Button label="π" />
+        <Button label="7" click={addNumber(7)} />
         <Button label="8" />
         <Button label="9" />
         <Button label="÷" />
